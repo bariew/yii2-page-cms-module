00b989cfcd4460a802ea9021ae861e337ca23752
Pavel Bariev <bariew@yandex.ru>
2014-05-30 11:57:00 +0400

    Added params folder

--- Module.php
+++ Module.php
@@ -4,5 +4,8 @@ namespace bariew\pageModule;
 
 class Module extends \yii\base\Module
 {
-
+    public function getParams()
+    {
+        return require __DIR__ . '/params/main.php';
+    }
 }
---
+++ params/main.php
@@ -0,0 +1,4 @@
+<?php  
+return [
+    'menu'  => ['label'    => 'Pages', 'url' => ['/page/item/index']]
+];
